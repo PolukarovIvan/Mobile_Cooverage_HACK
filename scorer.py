198340baa7829199cb1f4fac4a258cd69fc73176
pam4ek <47067030+pam4ek@users.noreply.github.com>
2021-08-15 15:05:50 +0300

    added coverage ratings
    data format:
    operator type_of_net x y score

--- scorer.py
+++ scorer.py
@@ -1,4 +1,4 @@
-import cv2, time
+import cv2, time, os
 
 def coverage_score(img_path, threshold = 1, verbose = False):
     start_time = time.time()
@@ -13,7 +13,42 @@ def coverage_score(img_path, threshold = 1, verbose = False):
         print(f'Done in {end_time-start_time} s')
     return score
 
+dir_path = os.path.join(os.curdir, 'scores')
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
 
-if __name__ == '__main__':
-    #print(coverage_score('image.jpg'))
-    pass
+dir_path = './scores'
+
+path_dir = ['/Bel_Tiles/lte',
+            '/Bel_Tiles/3g',
+            '/Meg_Tiles/3g',
+            '/Meg_Tiles/lte',
+            '/Mtc_Tiles/3g',
+            '/Mtc_Tiles/lte',
+            '/Tel_Tiles/3g',
+            '/Tel_Tiles/lte']
+
+start = time.time()
+for path in path_dir:
+    files_path = os.listdir('.'+path)
+    with open('./scores'+ path + '/scores.txt', 'w') as file:
+        file.write('')
+
+    for file_path in files_path:
+        path_to_file = '.'+path + '/' + file_path
+        print(path_to_file)
+        cur_score = coverage_score(path_to_file)
+        #print(cur_score)
+        if file_path[4:6] == '3g':
+            output =  file_path[:3] + ' ' + file_path[4:6] + ' ' + file_path[7:11] + ' ' +  file_path[12: 16] + ' ' + str(cur_score) + '\n'
+        else:
+            output = file_path[:3] + ' ' + file_path[4:7] + ' ' + file_path[8:12] + ' ' + file_path[13: 17] + ' ' + str(
+                cur_score) + '\n'
+        #print(output)
+        #print('./scores'+ path + '/scores.txt')
+        with open('./scores'+ path + '/scores.txt', 'a') as file:
+            file.write(output)
+
+
+end = time.time()
+print(f'Done in {end - start}')
